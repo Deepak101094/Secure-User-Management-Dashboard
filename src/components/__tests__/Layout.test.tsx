57652f065f80d65b324b6be46c92ab5a3ae3227f
Deepak/101094 <deepakpandey101094@gmail.com>
2024-02-23 17:01:18 +0530

    fix test issue

--- src/components/__tests__/Layout.test.tsx
+++ src/components/__tests__/Layout.test.tsx
@@ -1,12 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import Layout from "../Layout";
+import { store } from "../../redux/store";
 
 test("renders Layout component with Sidebar and Header", () => {
 	render(
-		<BrowserRouter>
-			<Layout>Test Content</Layout>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Layout>Test Content</Layout>
+			</BrowserRouter>
+		</Provider>
 	);
 
 	// Check if Header is rendered
